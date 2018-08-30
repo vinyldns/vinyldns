bbd10d40a5731151bc345f2b2717092bd08f1f83
Michael Ly <michaeltrulyng@gmail.com>
2018-08-30 00:45:16 -0400

    Convert TSIG key algoritm from default HMAC-MD5 to HMAC-SHA512.

--- modules/api/functional_test/conftest.py
+++ modules/api/functional_test/conftest.py
@@ -19,12 +19,14 @@ def pytest_addoption(parser):
                      help="The zone name that will be used for testing")
     parser.addoption("--dns-key-name", dest="dns_key_name", action="store", default="vinyldns.",
                      help="The name of the key used to sign updates for the zone")
-    parser.addoption("--dns-key", dest="dns_key", action="store", default="nzisn+4G2ldMn0q1CV3vsg==",
-                     help="The tsig key")
+    parser.addoption("--dns-key", dest="dns_key", action="store",
+                    default="wCZZS9lyRr77+jqfnkZ/92L9fD5ilmfrG0sslc3mgmTFsF1fRgmtJ0rj RkFITt8VHQ37wvM/nI9MAIWXYTvMqg==",
+                    help="The tsig key")
     parser.addoption("--dns-no-updates-key-name", dest="dns_no_updates_key_name", action="store",
                      default="vinyldns-no-updates", help="DDNS test TSIG key name")
     parser.addoption("--dns-no-updates-key", dest="dns_no_updates_key", action="store",
-                     default="BjI47RjxzSQyOktSnmbD4A==", help="DDNS test TSIG key")
+                     default="1GOhWm/nwqlQop1YQ6sl96eVTjULth0E7LonKB6X4uycygaCUQRG2JPQ kHVFgp768cyUuCv4j/tvL8C+cUCkcA==",
+                     help="DDNS test TSIG key")
 
     # optional
     parser.addoption("--basic-auth", dest="basic_auth_creds",
--- modules/api/functional_test/vinyldns_context.py
+++ modules/api/functional_test/vinyldns_context.py
@@ -4,9 +4,9 @@ class VinylDNSTestContext:
     dns_rev_v4_zone_name = '30.172.in-addr.arpa.'
     dns_rev_v6_zone_name = '1.9.e.f.c.c.7.2.9.6.d.f.ip6.arpa.'
     dns_key_name = 'vinyldns.'
-    dns_key = 'nzisn+4G2ldMn0q1CV3vsg=='
+    dns_key = 'wCZZS9lyRr77+jqfnkZ/92L9fD5ilmfrG0sslc3mgmTFsF1fRgmtJ0rj RkFITt8VHQ37wvM/nI9MAIWXYTvMqg=='
     dns_no_updates_key_name = 'vinyldns-no-updates'
-    dns_no_updates_key = 'BjI47RjxzSQyOktSnmbD4A=='
+    dns_no_updates_key = '1GOhWm/nwqlQop1YQ6sl96eVTjULth0E7LonKB6X4uycygaCUQRG2JPQ kHVFgp768cyUuCv4j/tvL8C+cUCkcA=='
     vinyldns_url = 'http://localhost:9000'
 
     @staticmethod
