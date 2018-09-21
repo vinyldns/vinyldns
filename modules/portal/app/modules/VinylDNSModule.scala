56746e9763251389c0bbb21d895a2c4e2da9c044
Rebecca Star <rebstar6@gmail.com>
2018-09-21 13:32:56 -0400

    Load configured repos in the portal (#229)
    * make portal dynamically connect to repos

--- modules/portal/app/modules/VinylDNSModule.scala
+++ modules/portal/app/modules/VinylDNSModule.scala
@@ -32,40 +32,33 @@ package modules
  * limitations under the License.
  */
 
-import cats.effect.IO
 import com.google.inject.AbstractModule
 import controllers._
+import controllers.repository.{PortalDataAccessor, PortalDataAccessorProvider}
 import play.api.{Configuration, Environment}
 import vinyldns.core.crypto.CryptoAlgebra
 import vinyldns.core.domain.membership.{UserChangeRepository, UserRepository}
-import vinyldns.dynamodb.repository.{
-  DynamoDBDataStoreSettings,
-  DynamoDBRepositorySettings,
-  DynamoDBUserChangeRepository,
-  DynamoDBUserRepository
-}
-
+import vinyldns.core.repository.DataStoreLoader
 class VinylDNSModule(environment: Environment, configuration: Configuration)
     extends AbstractModule {
 
   val settings = new Settings(configuration)
 
   def configure(): Unit = {
-    // Note: Leaving the unsafeRunSync here until we do full dynamic loading of the data store
-    val crypto = CryptoAlgebra.load(configuration.underlying.getConfig("crypto")).unsafeRunSync()
+    val startApp = for {
+      cryptoConf <- settings.cryptoConfig
+      crypto <- CryptoAlgebra.load(cryptoConf)
+      repoConfigs <- settings.dataStoreConfigs
+      repositories <- DataStoreLoader
+        .loadAll[PortalDataAccessor](repoConfigs, crypto, PortalDataAccessorProvider)
+    } yield {
+      bind(classOf[CryptoAlgebra]).toInstance(crypto)
+      bind(classOf[Authenticator]).toInstance(authenticator())
+      bind(classOf[UserRepository]).toInstance(repositories.userRepository)
+      bind(classOf[UserChangeRepository]).toInstance(repositories.userChangeRepository)
+    }
 
-    val dynamoConfig = configuration.get[Configuration]("dynamo")
-    val dynamoSettings = DynamoDBDataStoreSettings(
-      key = dynamoConfig.get[String]("key"),
-      secret = dynamoConfig.get[String]("secret"),
-      endpoint = dynamoConfig.get[String]("endpoint"),
-      region = dynamoConfig.get[String]("region")
-    )
-
-    bind(classOf[CryptoAlgebra]).toInstance(crypto)
-    bind(classOf[Authenticator]).toInstance(authenticator())
-    bind(classOf[UserRepository]).toInstance(userRepository(dynamoSettings, crypto))
-    bind(classOf[UserChangeRepository]).toInstance(changeLogStore(dynamoSettings, crypto))
+    startApp.unsafeRunSync()
   }
 
   private def authenticator(): Authenticator =
@@ -76,34 +69,4 @@ class VinylDNSModule(environment: Environment, configuration: Configuration)
       * to complete the Play framework upgrade
       */
     LdapAuthenticator(settings)
-
-  private def userRepository(
-      dynamoSettings: DynamoDBDataStoreSettings,
-      crypto: CryptoAlgebra): DynamoDBUserRepository = {
-    for {
-      repoSettings <- IO(
-        DynamoDBRepositorySettings(
-          tableName = configuration.get[String]("users.tablename"),
-          provisionedReads = configuration.get[Long]("users.provisionedReadThroughput"),
-          provisionedWrites = configuration.get[Long]("users.provisionedWriteThroughput")
-        )
-      )
-      repo <- DynamoDBUserRepository(repoSettings, dynamoSettings, crypto)
-    } yield repo
-  }.unsafeRunSync()
-
-  private def changeLogStore(
-      dynamoSettings: DynamoDBDataStoreSettings,
-      crypto: CryptoAlgebra): DynamoDBUserChangeRepository = {
-    for {
-      repoSettings <- IO(
-        DynamoDBRepositorySettings(
-          tableName = configuration.get[String]("changelog.tablename"),
-          provisionedReads = configuration.get[Long]("changelog.provisionedReadThroughput"),
-          provisionedWrites = configuration.get[Long]("changelog.provisionedWriteThroughput")
-        )
-      )
-      repo <- DynamoDBUserChangeRepository(repoSettings, dynamoSettings, crypto)
-    } yield repo
-  }.unsafeRunSync()
 }
