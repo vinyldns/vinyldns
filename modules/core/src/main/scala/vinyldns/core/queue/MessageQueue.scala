ceec91b5b602ec05d8b5177ac7109ab6ba74a228
Michael Ly <michaeltrulyng@gmail.com>
2018-10-01 16:58:00 -0400

    Updates based on feedback.

--- modules/core/src/main/scala/vinyldns/core/queue/MessageQueue.scala
+++ modules/core/src/main/scala/vinyldns/core/queue/MessageQueue.scala
@@ -1,40 +1,54 @@
+/*
+ * Copyright 2018 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vinyldns.core.queue
 import cats.effect.IO
-import vinyldns.core.domain.zone.ZoneCommand
+import vinyldns.core.domain.record.RecordSetChange
+import vinyldns.core.domain.zone.{ZoneChange, ZoneCommand}
 
 import scala.concurrent.duration.FiniteDuration
 
 // for ultimately acknowledging or re-queue of the message, this is queue implementation independent
 trait MessageHandle
-abstract class Message[A <: ZoneCommand] {
-  def handle: MessageHandle
-
-  // A vinyldns deserialized zone command
-  def command: A
-}
+sealed abstract class CommandMessage[A <: ZoneCommand](val handle: MessageHandle, val command: A) {}
+final case class RecordChangeMessage(h: MessageHandle, c: RecordSetChange) extends CommandMessage(h, c)
+final case class ZoneChangeMessage(h: MessageHandle, c: ZoneChange) extends CommandMessage(h, c)
 
 // main consumer
 trait MessageQueue {
 
   // receives a single message, the command should already be deserialized
-  def receive(): IO[Message[ZoneCommand]]
+  def receive(): IO[CommandMessage[_]]
 
   // receives a batch of messages
-  def receiveBatch(): IO[List[Message[ZoneCommand]]]
+  def receiveBatch(): IO[List[CommandMessage[_]]]
 
   // puts the message back on the queue with the intention of having it re-processed again
-  def requeue(message: Message[ZoneCommand]): IO[Unit]
+  def requeue(message: CommandMessage[_]): IO[Unit]
 
   // removes a message from the queue, indicating completion or the message should never be processed
-  def remove(message: Message[ZoneCommand]): IO[Unit]
+  def remove(message: CommandMessage[_]): IO[Unit]
 
   // alters the visibility timeout for a message on the queue.
-  def changeMessageTimeout(message: Message[ZoneCommand], duration: FiniteDuration): IO[Unit]
+  def changeMessageTimeout(message: CommandMessage[_], duration: FiniteDuration): IO[Unit]
 
   // sends a zone command to the queue, the queue will need to serialize it (if necessary) first
-  def send(command: ZoneCommand): IO[Unit]
+  def send[A <: ZoneCommand](command: A): IO[Unit]
 
   // sends a batch of messages to the queue, the queue should serialize those and return List[IO[Message]]
   // where each IO could individually fail.  Note: retry semantics is not a requirement of the queue implementation
-  def sendBatch(messages: List[ZoneCommand]): List[IO[Message[ZoneCommand]]]
+  def sendBatch(messages: List[ZoneCommand]): IO[Unit]
 }
--- modules/core/src/main/scala/vinyldns/core/queue/MessageQueueProvider.scala
+++ modules/core/src/main/scala/vinyldns/core/queue/MessageQueueProvider.scala
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2018 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vinyldns.core.queue
 import cats.effect.IO
 
 // follow data store loading on how to load the message queue settings using pure config, and how the
 // implementation loads and initializes itself.
 trait MessageQueueProvider[A] {
-  def load(settings: MessageQueueConfig): IO[MessageQueue[A]]
+  def load(settings: MessageQueueConfig): IO[MessageQueue]
 }
