82368d262065ca5349143c4f5208d54e8ab487dd
Nima Eskandary <nimaesk1@gmail.com>
2018-11-02 15:23:38 -0400

    membership repo

---
+++ modules/mysql/src/it/scala/vinyldns/mysql/repository/MySqlMembershipRepositoryIntegrationSpec.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2018 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vinyldns.mysql.repository
+
+import org.scalatest._
+import scalikejdbc.DB
+import vinyldns.core.domain.membership.MembershipRepository
+import vinyldns.mysql.TestMySqlInstance
+import scalikejdbc._
+
+class MySqlMembershipRepositoryIntegrationSpec
+  extends WordSpec
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Matchers
+    with Inspectors
+    with OptionValues {
+
+  private val repo: MembershipRepository = TestMySqlInstance.membershipRepository
+
+  def clear(): Unit =
+    DB.localTx { implicit s =>
+      s.executeUpdate("DELETE FROM membership")
+      ()
+    }
+
+  override protected def beforeEach(): Unit = clear()
+
+  override protected def afterAll(): Unit = clear()
+
+  private def generateUserIds(numUserIds: Int): Set[String] = {
+    val ids = for {
+      i <- 1 to numUserIds
+    } yield s"user-id-$i"
+
+    ids.toSet
+  }
+
+  private def getAllRecords: Set[(String, String)] =
+    DB.localTx { implicit s =>
+      sql"SELECT user_id, group_id FROM membership"
+        .map(res => Tuple2[String, String](res.string(1), res.string(2)))
+        .list()
+        .apply()
+        .toSet
+    }
+
+  "MySqlMembershipRepo.addMembers" should {
+    "add a member successfully" in {
+      val groupId = "group-id-1"
+      val userIds = Set("user-id-1")
+      val addResult = repo.addMembers(groupId, userIds).unsafeRunSync()
+
+      addResult should contain theSameElementsAs userIds
+
+      val getAllResult = getAllRecords
+      val expectedGetAllResult = Set(Tuple2(userIds.head, groupId))
+
+      getAllResult should contain theSameElementsAs expectedGetAllResult
+    }
+
+    "add multiple members successfully" in {
+      val groupId = "group-id-1"
+      val userIds = generateUserIds(10)
+      val addResult = repo.addMembers(groupId, userIds).unsafeRunSync()
+
+      addResult should contain theSameElementsAs userIds
+
+      val expectedGetAllResult = userIds.map(Tuple2(_, groupId))
+
+      getAllRecords should contain theSameElementsAs expectedGetAllResult
+    }
+
+    "add a group for an existing user" in {
+      val groupIdOne = "group-id-1"
+      val groupIdTwo = "group-id-2"
+      val userIds = Set("user-id-1")
+
+      repo.addMembers(groupIdOne, userIds).unsafeRunSync() shouldBe userIds
+      repo.addMembers(groupIdTwo, userIds).unsafeRunSync() shouldBe userIds
+
+      val expectedGroups = Set(groupIdOne, groupIdTwo)
+      repo.getGroupsForUser(userIds.head).unsafeRunSync() should contain theSameElementsAs expectedGroups
+    }
+  }
+
+  "MySqlMembershipRepo.removeMembers" should {
+    "remove a member successfully from group" in {
+      val groupId = "group-id-1"
+      val userIds = Set("user-id-1")
+
+      repo.addMembers(groupId, userIds).unsafeRunSync()
+      getAllRecords should contain theSameElementsAs Set(Tuple2(userIds.head, groupId))
+
+      repo.removeMembers(groupId, userIds).unsafeRunSync() should contain theSameElementsAs userIds
+      getAllRecords shouldBe Set()
+    }
+
+    "remove multiple members successfully from group" in {
+      val groupId = "group-id-1"
+      val userIds = generateUserIds(10)
+
+      repo.addMembers(groupId, userIds).unsafeRunSync()
+      getAllRecords should contain theSameElementsAs userIds.map(Tuple2(_, groupId))
+
+      val toBeRemoved = userIds.take(5)
+      repo.removeMembers(groupId, toBeRemoved).unsafeRunSync() should contain theSameElementsAs toBeRemoved
+      val expectedUserIds = userIds -- toBeRemoved
+      getAllRecords should contain theSameElementsAs expectedUserIds.map(Tuple2(_, groupId))
+    }
+  }
+
+  "MySqlMembershipRepo.getGroupsForUser" should {
+    "get all groups for user" in {
+      val noisyIds = generateUserIds(10)
+      val groupIdOne = "group-id-1"
+      val groupIdTwo = "group-id-2"
+      val groupIdThree = "group-id-3"
+
+      // make some noise
+      repo.addMembers(groupIdOne, noisyIds).unsafeRunSync()
+      repo.addMembers(groupIdTwo, noisyIds).unsafeRunSync()
+      repo.addMembers(groupIdThree, noisyIds).unsafeRunSync()
+
+      val underTest = Set("user-id-under-test")
+      repo.addMembers(groupIdOne, underTest).unsafeRunSync()
+      repo.addMembers(groupIdTwo, underTest).unsafeRunSync()
+      // not adding to group three
+
+      val expectedGroups = Set(groupIdOne, groupIdTwo)
+      repo.getGroupsForUser(underTest.head).unsafeRunSync() should contain theSameElementsAs expectedGroups
+    }
+
+    "return empty when no groups for user" in {
+      val groupId = "group-id-1"
+      val noisyIds = generateUserIds(2)
+      repo.addMembers(groupId, noisyIds).unsafeRunSync()
+
+      val underTest = Set("user-id-under-test")
+      repo.getGroupsForUser(underTest.head).unsafeRunSync() shouldBe Set()
+    }
+  }
+
+}
---
+++ modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlMembershipRepository.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vinyldns.mysql.repository
+
+import cats.effect.IO
+import cats.implicits._
+import org.slf4j.LoggerFactory
+import vinyldns.core.domain.membership.MembershipRepository
+import vinyldns.core.route.Monitored
+import scalikejdbc._
+
+class MySqlMembershipRepository extends MembershipRepository with Monitored {
+  private final val logger = LoggerFactory.getLogger(classOf[MySqlMembershipRepository])
+
+  private final val BASE_ADD_MEMBERS = "REPLACE INTO membership (user_id, group_id)"
+
+  private final val BASE_REMOVE_MEMBERS = "DELETE FROM membership WHERE group_id = ?"
+
+  private final val GET_GROUPS_FOR_USER =
+    sql"""
+      |SELECT group_id
+      |  FROM membership
+      | WHERE user_id = ?
+    """.stripMargin
+
+  def addMembers(groupId: String, memberUserIds: Set[String]): IO[Set[String]] =
+    monitor("repo.Membership.addMembers") {
+      IO {
+        logger.info(s"Saving into group $groupId members $memberUserIds")
+        DB.localTx { implicit s =>
+          val memberUserIdList = memberUserIds.toList
+          val valueClause = " VALUES " + memberUserIdList.as("(?, ?)").mkString(",")
+          val query = BASE_ADD_MEMBERS + valueClause
+          val valueParams: List[String] = memberUserIdList.flatMap(Seq(_, groupId))
+          SQL(query)
+            .bind(valueParams: _*)
+            .update
+            .apply()
+
+          memberUserIds
+        }
+      }
+    }
+
+  def removeMembers(groupId: String, memberUserIds: Set[String]): IO[Set[String]] =
+    monitor("repo.Membership.removeMembers") {
+      IO {
+        logger.info(s"Removing from group $groupId members $memberUserIds")
+        DB.localTx { implicit s =>
+          val memberUserIdList = memberUserIds.toList
+          val inClause = " AND user_id IN (" + memberUserIdList.as("?").mkString(",") + ")"
+          val query = BASE_REMOVE_MEMBERS + inClause
+          SQL(query)
+            .bind(groupId :: memberUserIdList: _*)
+            .update()
+            .apply()
+
+          memberUserIds
+        }
+      }
+    }
+
+  def getGroupsForUser(userId: String): IO[Set[String]] =
+    monitor("repo.Membership.getGroupsForUser") {
+      IO {
+        logger.info(s"Getting groups for user $userId")
+        DB.readOnly { implicit s =>
+          GET_GROUPS_FOR_USER
+            .bind(userId)
+            .map(_.string(1))
+            .list()
+            .apply()
+            .toSet
+        }
+      }
+    }
+}
