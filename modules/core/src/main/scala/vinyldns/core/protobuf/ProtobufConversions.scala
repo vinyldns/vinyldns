19c6abe4b514a64e1f5be9513561c0372de07c23
Rebecca Star <rebstar6@gmail.com>
2019-01-24 13:43:38 -0500

    with tests

--- modules/core/src/main/scala/vinyldns/core/protobuf/ProtobufConversions.scala
+++ modules/core/src/main/scala/vinyldns/core/protobuf/ProtobufConversions.scala
@@ -283,7 +283,7 @@ trait ProtobufConversions {
 
   /* This cannot be called toPB because RecordData is the base type for things like AData, cannot overload */
   def toRecordData(data: RecordData): VinylDNSProto.RecordData = {
-    val d = (data: @unchecked) match {
+    val d = data match {
       case x: AData => toPB(x)
       case x: AAAAData => toPB(x)
       case x: CNAMEData => toPB(x)
