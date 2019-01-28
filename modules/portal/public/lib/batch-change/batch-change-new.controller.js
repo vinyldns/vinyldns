f0f174a991ee3adfd256359783900566a3951c35
Britney Wright <blw06g@gmail.com>
2019-01-28 17:05:08 -0500

    move record owner group field to top of batch

--- modules/portal/public/lib/batch-change/batch-change-new.controller.js
+++ modules/portal/public/lib/batch-change/batch-change-new.controller.js
@@ -19,12 +19,14 @@
 
     angular.module('batch-change')
         .controller('BatchChangeNewController', function($scope, jsConfig, $log, $location, $timeout, batchChangeService, utilityService, groupsService){
-
             $scope.load = function(){
-                groupsService.getMyGroupsStored().then(
-                    function (results) {
+                groupsService.getMyGroupsStored()
+                    .then(function (results) {
                         $scope.myGroups = results['groups'];
                     })
+                    .catch(function (error) {
+                        handleError(error, 'groupsService::getMyGroupsStored-failure');
+                    });
             }
 
             $scope.batch = {};
