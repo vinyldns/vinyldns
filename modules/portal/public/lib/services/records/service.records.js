2cd18da25fb1ce1f6778abcc1ead89f5d6d057fe
Michael Ly <michaeltrulyng@gmail.com>
2019-02-15 11:19:33 -0500

    Portal support for DS records (#484)
    * Add DS to ACL support.

--- modules/portal/public/lib/services/records/service.records.js
+++ modules/portal/public/lib/services/records/service.records.js
@@ -100,16 +100,6 @@ angular.module('service.records', [])
                     });
                     newRecord.onlyFour = true;
                     break;
-                case 'CNAME':
-                    newRecord.cnameRecordData = record.records[0].cname;
-                    break;
-                case 'MX':
-                    newRecord.mxItems = [];
-                    angular.forEach(record.records, function(item) {
-                        newRecord.mxItems.push(item);
-                    });
-                    newRecord.onlyFour = true;
-                    break;
                 case 'AAAA':
                     newRecord.aaaaRecordData = [];
                     angular.forEach(record.records, function(aaaaRecord) {
@@ -117,8 +107,22 @@ angular.module('service.records', [])
                     });
                     newRecord.onlyFour = true;
                     break;
-                case 'TXT':
-                    newRecord.textRecordData = record.records[0].text;
+                case 'CNAME':
+                    newRecord.cnameRecordData = record.records[0].cname;
+                    break;
+                case 'DS':
+                    newRecord.dsKeyTag = record.records[0].keytag;
+                    newRecord.dsAlgorithm = record.records[0].algorithm;
+                    newRecord.dsDigestType = record.records[0].digesttype;
+                    newRecord.dsDigest = record.records[0].digest;
+                    newRecord.canBeEdited = false;
+                    break;
+                case 'MX':
+                    newRecord.mxItems = [];
+                    angular.forEach(record.records, function(item) {
+                        newRecord.mxItems.push(item);
+                    });
+                    newRecord.onlyFour = true;
                     break;
                 case 'NS':
                     newRecord.nsRecordData = [];
@@ -145,17 +149,17 @@ angular.module('service.records', [])
                     newRecord.soaMinimum = record.records[0].minimum;
                     newRecord.canBeEdited = false;
                     break;
-                case 'SRV':
-                    newRecord.srvItems = [];
-                    angular.forEach(record.records, function(item) {
-                        newRecord.srvItems.push(item);
-                    });
-                    newRecord.onlyFour = true;
-                    break;
                 case 'SPF':
                     newRecord.spfRecordData = [];
                     angular.forEach(record.records, function(spfRecord) {
                         newRecord.spfRecordData.push(spfRecord.text);
+                    });
+                    newRecord.onlyFour = true;
+                    break;
+                case 'SRV':
+                    newRecord.srvItems = [];
+                    angular.forEach(record.records, function(item) {
+                        newRecord.srvItems.push(item);
                     });
                     newRecord.onlyFour = true;
                     break;
@@ -165,6 +169,9 @@ angular.module('service.records', [])
                         newRecord.sshfpItems.push(item);
                     });
                     newRecord.onlyFour = true;
+                    break;
+                case 'TXT':
+                    newRecord.textRecordData = record.records[0].text;
                     break;
                 default:
                     newRecord.canBeEdited = false;
@@ -186,6 +193,12 @@ angular.module('service.records', [])
                         newRecord.records.push({"address": address});
                     });
                     break;
+                case 'AAAA':
+                    newRecord.records = [];
+                    angular.forEach(record.aaaaRecordData, function(address) {
+                        newRecord.records.push({"address": address});
+                    });
+                    break;
                 case 'CNAME':
                     newRecord.records = [{"cname": record.cnameRecordData}];
                     break;
@@ -195,14 +208,11 @@ angular.module('service.records', [])
                         newRecord.records.push({"preference": Number(record.preference), "exchange": record.exchange})
                     });
                     break;
-                case 'AAAA':
+                case 'NS':
                     newRecord.records = [];
-                    angular.forEach(record.aaaaRecordData, function(address) {
-                        newRecord.records.push({"address": address});
+                    angular.forEach(record.nsRecordData, function(address) {
+                      newRecord.records.push({"nsdname": address});
                     });
-                    break;
-                case 'TXT':
-                    newRecord.records = [{"text": record.textRecordData}];
                     break;
                 case 'PTR':
                     newRecord.records = [];
@@ -232,11 +242,8 @@ angular.module('service.records', [])
                             "fingerprint": record.fingerprint})
                     });
                     break;
-                case 'NS':
-                    newRecord.records = [];
-                    angular.forEach(record.nsRecordData, function(address) {
-                      newRecord.records.push({"nsdname": address});
-                    });
+                case 'TXT':
+                    newRecord.records = [{"text": record.textRecordData}];
                     break;
                 default:
             }
@@ -245,6 +252,4 @@ angular.module('service.records', [])
             }
             return newRecord;
         };
-
-
     });
