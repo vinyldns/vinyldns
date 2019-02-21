a040ae4a9d689c77060843e1b11ea0b7c9112e01
Rebecca Star <rebstar6@gmail.com>
2019-02-21 10:46:46 -0500

    add extra level of safety around zone deletes

--- modules/api/src/main/scala/vinyldns/api/repository/TestDataLoader.scala
+++ modules/api/src/main/scala/vinyldns/api/repository/TestDataLoader.scala
@@ -197,7 +197,13 @@ object TestDataLoader {
               (test.name == nonTestSharedZone.name && test.adminGroupId == nonTestSharedZone.adminGroupId) =>
           test.copy(status = ZoneStatus.Deleted)
       }.toList
-      _ <- IO(logger.info(s"Deleting existing shared zones on startup: ${toDelete.map(z => (z.name, z.id))}"))
+      _ <- if (toDelete.length > 2) {
+        logger.error(s"Unexpected zones to delete on startup: ${toDelete.map(z => (z.name, z.id))}")
+        IO.raiseError(new RuntimeException("Startup attempting to delete too many zones"))
+      } else {
+        logger.info(s"Deleting existing shared zones on startup: ${toDelete.map(z => (z.name, z.id))}")
+        IO.unit
+      }
       _ <- toDelete.map(zoneRepo.save).parSequence
       _ <- groupRepo.save(sharedZoneGroup)
       _ <- membershipRepo.addMembers(
