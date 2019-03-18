892675800b2bc938fe18825b638e88dfd092437d
Nima Eskandary <nimaesk1@gmail.com>
2019-03-18 11:00:49 -0400

    michael

--- modules/portal/dist/wait-for-dependencies.sh
+++ modules/portal/dist/wait-for-dependencies.sh
@@ -8,12 +8,12 @@ fi
 # the mysql address, default to a local docker setup
 MYSQL_ADDRESS=${MYSQL_ADDRESS:-vinyldns-mysql}
 MYSQL_PORT=${MYSQL_PORT:-3306}
-echo "Waiting for MYSQL to be ready on $MYSQL_ADDRESS:$MYSQL_PORT"
+echo "Waiting for MYSQL to be ready on ${MYSQL_ADDRESS}:${MYSQL_PORT}"
 DATA=""
 RETRY=30
 while [ $RETRY -gt 0 ]
 do
-    DATA=$(nc -vzw1 $MYSQL_ADDRESS $MYSQL_PORT)
+    DATA=$(nc -vzw1 "$MYSQL_ADDRESS" "$MYSQL_PORT")
     if [ $? -eq 0 ]
     then
         break
@@ -25,7 +25,7 @@ do
 
         if [ $RETRY -eq 0 ]
         then
-          echo "Exceeded retries waiting for MYSQL to be ready on $MYSQL_ADDRESS:$MYSQL_PORT, failing"
+          echo "Exceeded retries waiting for MYSQL to be ready on ${MYSQL_ADDRESS}:${MYSQL_PORT}, failing"
           return 1
         fi
     fi
