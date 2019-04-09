7571450384334b19515cfe8931128e682471dcc8
Britney Wright <blw06g@gmail.com>
2019-04-09 15:43:28 -0400

    assert expected record data

--- modules/api/functional_test/live_tests/recordsets/create_recordset_test.py
+++ modules/api/functional_test/live_tests/recordsets/create_recordset_test.py
@@ -1517,6 +1517,7 @@ def test_create_long_txt_record_succeeds(shared_zone_test_context):
     try:
         rs_create = client.create_recordset(long_txt_rs, status=202)
         rs = client.wait_until_recordset_change_status(rs_create, 'Complete')['recordSet']
+        assert_that(rs['records'][0]['text'], is_(record_data))
     finally:
         try:
             delete_result = client.delete_recordset(rs['zoneId'], rs['id'], status=202)
