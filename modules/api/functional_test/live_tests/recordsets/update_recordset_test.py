56fde0edb445089ccc15ab1913d4f619ba8031e1
Nima Eskandary <nimaesk1@gmail.com>
2019-04-29 15:05:19 -0400

    more accurately check regex

--- modules/api/functional_test/live_tests/recordsets/update_recordset_test.py
+++ modules/api/functional_test/live_tests/recordsets/update_recordset_test.py
@@ -1959,11 +1959,11 @@ def test_update_high_value_domain_fails_case_insensitive(shared_zone_test_contex
     client = shared_zone_test_context.ok_vinyldns_client
     zone_system = shared_zone_test_context.system_test_zone
     list_results_page_system = client.list_recordsets(zone_system['id'],  status=200)['recordSets']
-    record_system = [item for item in list_results_page_system if item['name'] == 'high-value-domain-UPPER-CASE'][0]
+    record_system = [item for item in list_results_page_system if item['name'] == 'high-VALUE-domain-UPPER-CASE'][0]
     record_system['ttl'] = record_system['ttl'] + 100
 
     errors_system = client.update_recordset(record_system, status=422)
-    assert_that(errors_system, is_('Record name "high-value-domain-UPPER-CASE.system-test." is configured as a High Value Domain, so it cannot be modified.'))
+    assert_that(errors_system, is_('Record name "high-VALUE-domain-UPPER-CASE.system-test." is configured as a High Value Domain, so it cannot be modified.'))
 
 
 def test_update_high_value_domain_fails_ip4_ptr(shared_zone_test_context):
