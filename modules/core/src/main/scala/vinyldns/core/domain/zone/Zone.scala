96a0b4160c35a5ffa584da89045570c8985c242b
Nima Eskandary <nimaesk1@gmail.com>
2019-04-30 14:18:42 -0400

    default for tests

--- modules/core/src/main/scala/vinyldns/core/domain/zone/Zone.scala
+++ modules/core/src/main/scala/vinyldns/core/domain/zone/Zone.scala
@@ -76,7 +76,7 @@ final case class Zone(
 }
 
 object Zone {
-  def apply(createZoneInput: CreateZoneInput, isTest: Boolean): Zone = {
+  def apply(createZoneInput: CreateZoneInput, isTest: Boolean = false): Zone = {
     import createZoneInput._
 
     Zone(
