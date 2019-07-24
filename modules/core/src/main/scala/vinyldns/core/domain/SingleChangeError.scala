5aaac6671b30c3940c26558c093432e44b98f7f2
Paul James Cleary <pauljamescleary@gmail.com>
2019-07-24 09:21:04 -0400

    Moving ScheduledChangeDisabled to BCER

--- modules/core/src/main/scala/vinyldns/core/domain/SingleChangeError.scala
+++ modules/core/src/main/scala/vinyldns/core/domain/SingleChangeError.scala
@@ -34,7 +34,7 @@ object DomainValidationErrorType extends Enumeration {
   InvalidBatchRecordType, ZoneDiscoveryError, RecordAlreadyExists, RecordDoesNotExist,
   CnameIsNotUniqueError, UserIsNotAuthorized, RecordNameNotUniqueInBatch, RecordInReverseZoneError,
   HighValueDomainError, MissingOwnerGroupId, ExistingMultiRecordError, NewMultiRecordError,
-  CnameAtZoneApexError, ScheduledChangeDisabled = Value
+  CnameAtZoneApexError = Value
 
   // $COVERAGE-OFF$
   def from(error: DomainValidationError): DomainValidationErrorType =
@@ -66,7 +66,6 @@ object DomainValidationErrorType extends Enumeration {
       case _: ExistingMultiRecordError => ExistingMultiRecordError
       case _: NewMultiRecordError => NewMultiRecordError
       case _: CnameAtZoneApexError => CnameAtZoneApexError
-      case ScheduledChangesDisabled => ScheduledChangeDisabled
     }
   // $COVERAGE-ON$
 }
