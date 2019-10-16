4e7f54ad76d305e0d155542b38cd93ca34818715
Paul James Cleary <pauljamescleary@gmail.com>
2019-10-16 10:07:41 -0400

    Cleaning up

--- build/build.sh
+++ build/build.sh
@@ -6,13 +6,16 @@ function usage {
     printf "usage: build.sh [OPTIONS]\n\n"
     printf "builds vinyldns artifacts locally\n\n"
     printf "options:\n"
+    printf "\t-b, --branch [BRANCH]: the git branch or tag to build\n"
     printf "\t-v, --version [VERSION]: the version given to this build\n"
 }
 
 # parse args
 VINYLDNS_VERSION=
+BRANCH="master"
 while [ "$1" != "" ]; do
 	case "$1" in
+		-b | --branch  	) BRANCH="$2";  shift;;
 		-v | --version  	) VINYLDNS_VERSION="$2";  shift;;
 		* ) usage; exit 1;;
 	esac
@@ -21,15 +24,17 @@ done
 
 export VINYLDNS_VERSION=$VINYLDNS_VERSION
 echo "BUILDING $VINYLDNS_VERSION..."
-set -x
+
 # Builds the images
 docker-compose -f $CURDIR/docker/docker-compose.yml build \
   --no-cache \
   --parallel \
   --build-arg VINYLDNS_VERSION="$VINYLDNS_VERSION" \
-  --build-arg BRANCH="master"
-set +x
-if [ $? -eq 0 ]; then
+  --build-arg BRANCH="${BRANCH}"
+
+if [ ? -eq 0 ]; then
   # Runs smoke tests to make sure the new images are sound
   docker-compose -f $CURDIR/docker/docker-compose.yml --log-level ERROR up --exit-code-from functest
 fi
+
+
--- build/release.sh
+++ build/release.sh
@@ -6,21 +6,26 @@ function usage {
     printf "usage: build.sh [OPTIONS]\n\n"
     printf "builds vinyldns artifacts locally\n\n"
     printf "options:\n"
-    printf "\t-t, --tag [TAG]: sets the qualifier for the semver version\n"
+    printf "\t-b, --build: indicates a fresh build or attempt to work with existing images\n"
+    printf "\t-p, --push: indicates docker will push to the repository\n"
     printf "\t-r, --repository [REPOSITORY]: the docker repository where this image will be pushed\n"
+    printf "\t-t, --tag [TAG]: sets the qualifier for the semver version\n"
 }
 
 # Default the build to -SNAPSHOT if not set
 BUILD_TAG="-SNAPSHOT"
 REPOSITORY="docker.io"
+DOCKER_PUSH=0
+DO_BUILD=0
 
 while [ "$1" != "" ]; do
 	case "$1" in
-		-t | --tag  	) BUILD_TAG="-b$2";  shift;;
-		-r | --repository  	) REPOSITORY="$2";  shift;;
+    -b | --build  	) DO_BUILD=1;  shift;;
+    -p | --push  	) DOCKER_PUSH=1;  shift;;
+		-r | --repository  	) REPOSITORY="$2";  shift 2;;
+		-t | --tag  	) BUILD_TAG="-b$2";  shift 2;;
 		* ) usage; exit;;
 	esac
-	shift
 done
 
 BASEDIR=$CURDIR/../
@@ -35,15 +40,24 @@ else
   export VINYLDNS_VERSION="$V${BUILD_TAG}"
 fi
 
-./build.sh --version "${VINYLDNS_VERSION}"
+if [ $DO_BUILD -eq 1 ]; then
+  ./build.sh --version "${VINYLDNS_VERSION}"
 
-if [ $? -eq 0 ]; then
-  echo "VERSION = $VINYLDNS_VERSION"
+  if [ $? -eq 0 ]; then
+    set -x
+    docker tag vinyldns/test-bind9:$VINYLDNS_VERSION $REPOSITORY/vinyldns/test-bind9:$VINYLDNS_VERSION
+    docker tag vinyldns/test:$VINYLDNS_VERSION $REPOSITORY/vinyldns/test:$VINYLDNS_VERSION
+    docker tag vinyldns/api:$VINYLDNS_VERSION $REPOSITORY/vinyldns/api:$VINYLDNS_VERSION
+    docker tag vinyldns/portal:$VINYLDNS_VERSION $REPOSITORY/vinyldns/portal:$VINYLDNS_VERSION
+    set +x
+  fi
+fi
 
+if [ $DOCKER_PUSH -eq 1 ]; then
   set -x
-  docker tag vinyldns/test-bind9:$VINYLDNS_VERSION $REPOSITORY/vinyldns/test-bind9:$VINYLDNS_VERSION
-  docker tag vinyldns/test:$VINYLDNS_VERSION $REPOSITORY/vinyldns/test:$VINYLDNS_VERSION
-  docker tag vinyldns/api:$VINYLDNS_VERSION $REPOSITORY/vinyldns/api:$VINYLDNS_VERSION
-  docker tag vinyldns/portal:$VINYLDNS_VERSION $REPOSITORY/vinyldns/portal:$VINYLDNS_VERSION
+  docker push $REPOSITORY/vinyldns/test-bind9:$VINYLDNS_VERSION
+  docker push $REPOSITORY/vinyldns/test:$VINYLDNS_VERSION
+  docker push $REPOSITORY/vinyldns/api:$VINYLDNS_VERSION
+  docker push $REPOSITORY/vinyldns/portal:$VINYLDNS_VERSION
   set +x
 fi
