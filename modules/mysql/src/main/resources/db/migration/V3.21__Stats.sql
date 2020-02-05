3093435a71f8241abbcc21df1c97095509a647ac
Britney Wright <blw06g@gmail.com>
2020-02-05 09:51:32 -0500

    update stats table

--- modules/mysql/src/main/resources/db/migration/V3.21__Stats.sql
+++ modules/mysql/src/main/resources/db/migration/V3.21__Stats.sql
@@ -3,10 +3,11 @@ CREATE SCHEMA IF NOT EXISTS ${dbName};
 USE ${dbName};
 
 CREATE TABLE stats (
-  id INT NOT NULL AUTO_INCREMENT,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   count BIGINT NOT NULL,
   created DATETIME NOT NULL,
   PRIMARY KEY (id),
-  INDEX stats_name_index (name)
+  INDEX stats_name_index (name),
+  INDEX stats_name_created_index (name ASC, created ASC)
 );
