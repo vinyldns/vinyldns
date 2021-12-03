bb81dad224498dc0e4c5984b1845cae7e5577cf5
Emerle, Ryan <Ryan_Emerle@comcast.com>
2021-12-03 16:21:37 -0500

    Fix for code coverage output

--- build/verify.sh
+++ build/verify.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-cd "$DIR/../test/api/integration"
-make build && make run WITH_ARGS="bash -c \"sbt ';validate' && sbt ';verify'\""
+cd "${DIR}/../test/api/integration"
+make build && make run DOCKER_PARAMS="-v \"$(pwd)/../../../target:/build/target\"" WITH_ARGS="bash -c \"sbt ';validate' && sbt ';verify'\""
