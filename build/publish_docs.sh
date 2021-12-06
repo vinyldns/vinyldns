9a6350acef7fbfd379d2064530d02f8403ccead4
Ryan Emerle <Ryan_Emerle@comcast.com>
2021-12-06 16:21:44 -0500

    Update publish_docs [ci skip]

--- build/publish_docs.sh
+++ build/publish_docs.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-docker run -i --rm -e RUN_SERVICES=none -e SBT_MICROSITES_PUBLISH_TOKEN="${SBT_MICROSITES_PUBLISH_TOKEN}" -v "${DIR}/../:/build" vinyldns/build:base-build-docs /bin/bash -c "sbt ';project docs; publishMicrosite'"
+USE_TTY="" && test -t 1 && USE_TTY="-t"
+docker run -i ${USE_TTY} --rm -e RUN_SERVICES=none -e SBT_MICROSITES_PUBLISH_TOKEN="${SBT_MICROSITES_PUBLISH_TOKEN}" -v "${DIR}/../:/build" vinyldns/build:base-build-docs /bin/bash -c "sbt ';project docs; publishMicrosite'"
