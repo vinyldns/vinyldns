e9cdeef7247bd6c63216749dac13bd411817a5d2
Jay07GIT <jeyraj931@gmail.com>
2022-04-20 10:08:51 +0530

    update

--- modules/mysql/src/main/resources/db/migration/V3.26__RecordSetData_RecordName_OwnerGroup.sql
+++ modules/mysql/src/main/resources/db/migration/V3.26__RecordSetData_RecordName_OwnerGroup.sql
@@ -9,4 +9,4 @@ ALTER TABLE recordset_data ADD COLUMN owner_group_id CHAR(36) AFTER ip;
 CREATE INDEX recordset_data_owner_group_id_index ON recordset_data (owner_group_id);
 
 ALTER TABLE recordset_data DROP INDEX recordset_data_fdqn_fulltext_index;
-ALTER TABLE recordset_data DROP INDEX recordset_data_reverse_fqdn_fulltext_index;
+ALTER TABLE recordset_data RENAME INDEX recordset_data_record_data_fulltex_index TO recordset_data_record_data_fulltext_index
