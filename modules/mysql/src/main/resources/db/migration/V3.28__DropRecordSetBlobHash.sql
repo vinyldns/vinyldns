6f1b9ba8ae26ea8b1aab78c34b21a3356e6c0bc6
Ryan Emerle <Ryan_Emerle@comcast.com>
2022-04-25 09:10:51 -0400

    Remove the original migration change

--- modules/mysql/src/main/resources/db/migration/V3.28__DropRecordSetBlobHash.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS ${dbName};
-
-USE ${dbName};
-
-/*
-Removing recordSet blob hash column and index in recordset table as its been no longer use
-*/
-
-ALTER TABLE recordset DROP COLUMN data_hash;
