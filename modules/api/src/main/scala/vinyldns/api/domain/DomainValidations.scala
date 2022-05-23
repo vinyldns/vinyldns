ad0a37236a48fafd02d35cce9a7cba8ab66cc640
Jay07GIT <jeyraj931@gmail.com>
2022-05-23 13:12:33 +0530

    Added forward slash validation for forward and reverse zone

--- modules/api/src/main/scala/vinyldns/api/domain/DomainValidations.scala
+++ modules/api/src/main/scala/vinyldns/api/domain/DomainValidations.scala
@@ -27,8 +27,10 @@ import scala.util.matching.Regex
   Object to house common domain validations
  */
 object DomainValidations {
+  val validReverseFQDNRegex: Regex =
+    """^(?:([0-9a-zA-Z\-\/_]{1,63}|[0-9a-zA-Z\-\/_]{1}[0-9a-zA-Z\-\/_]{0,61}[0-9a-zA-Z\-\/_]{1}|[*.]{2}[0-9a-zA-Z\-\/_]{0,60}[0-9a-zA-Z\-\/_]{1})\.)*$""".r
   val validFQDNRegex: Regex =
-    """^(?:([0-9a-zA-Z_]{1,63}|[0-9a-zA-Z_]{1}[0-9a-zA-Z\-\/_]{0,61}[0-9a-zA-Z_]{1}|[*.]{2}[0-9a-zA-Z\-\/_]{0,60}[0-9a-zA-Z_]{1})\.)*$""".r
+    """^(?:([0-9a-zA-Z_]{1,63}|[0-9a-zA-Z_]{1}[0-9a-zA-Z\-_]{0,61}[0-9a-zA-Z_]{1}|[*.]{2}[0-9a-zA-Z\-_]{0,60}[0-9a-zA-Z_]{1})\.)*$""".r
   val validIpv4Regex: Regex =
     """^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$""".r
   val validIpv6Regex: Regex =
@@ -60,7 +62,35 @@ object DomainValidations {
   def validateHostName(name: Fqdn): ValidatedNel[DomainValidationError, Fqdn] =
     validateHostName(name.fqdn).map(_ => name)
 
+  def validateReverseHostName(name: Fqdn): ValidatedNel[DomainValidationError, Fqdn] =
+    validateReverseHostName(name.fqdn).map(_ => name)
+
   def validateHostName(name: String): ValidatedNel[DomainValidationError, String] = {
+    /*
+      Label rules are as follows (from RFC 952; detailed in RFC 1034):
+        - Starts with a letter, or digit, or underscore or asterisk (as of RFC 1123)
+        - Interior contains letter, digit or hyphen, or underscore
+        - Ends with a letter or digit, or underscore
+      All possible labels permutations:
+        - A single letter/digit: [0-9a-zA-Z]{1}
+        - A combination of 1-63 letters/digits: [0-9a-zA-Z]{1,63}
+        - A single letter/digit followed by up to 61 letters, digits, hyphens or slashes
+        and ending with a letter/digit:[0-9a-zA-Z]{1}[0-9a-zA-Z\-]{0,61}[0-9a-zA-Z]{1}
+        - A wildcard and dot character (*.) followed by up to 60 letters, digits, hyphens
+        and ending with a letter/digit:[*.]{2}[0-9a-zA-Z\-_]{0,60}[0-9a-zA-Z_]{1}
+      A valid domain name is a series of one or more <label>s,
+      joined by dots and terminating on a zero-length <label> (ie. dot)
+     */
+    val checkRegex = validFQDNRegex
+      .findFirstIn(name)
+      .map(_.validNel)
+      .getOrElse(InvalidDomainName(name).invalidNel)
+    val checkLength = validateStringLength(name, Some(HOST_MIN_LENGTH), HOST_MAX_LENGTH)
+
+    checkRegex.combine(checkLength).map(_ => name)
+  }
+
+  def validateReverseHostName(name: String): ValidatedNel[DomainValidationError, String] = {
     /*
       Label rules are as follows (from RFC 952; detailed in RFC 1034):
         - Starts with a letter, or digit, or underscore or asterisk (as of RFC 1123)
@@ -76,7 +106,7 @@ object DomainValidations {
       A valid domain name is a series of one or more <label>s,
       joined by dots/slashes and terminating on a zero-length <label> (ie. dot)
      */
-    val checkRegex = validFQDNRegex
+    val checkRegex = validReverseFQDNRegex
       .findFirstIn(name)
       .map(_.validNel)
       .getOrElse(InvalidDomainName(name).invalidNel)
