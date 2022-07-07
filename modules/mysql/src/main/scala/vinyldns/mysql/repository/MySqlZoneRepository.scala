a61cf559f1c0e8dac023bedb107de987feddce5c
Nicholas Spadaccino <nicholas_spadaccino@comcast.com>
2022-07-07 11:51:05 -0400

    Revert listzones sql query change

--- modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlZoneRepository.scala
+++ modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlZoneRepository.scala
@@ -254,7 +254,7 @@ class MySqlZoneRepository extends ZoneRepository with ProtobufConversions with M
           sb.append(withAccessorCheck)
 
           val filters = List(
-            zoneNameFilter.map(flt => s"LOWER ( z.name ) LIKE '${ensureTrailingDot(flt.replace('*', '%').toLowerCase())}'"),
+            zoneNameFilter.map(flt => s"z.name LIKE '${ensureTrailingDot(flt.replace('*', '%'))}'"),
             startFrom.map(os => s"z.name > '$os'")
           ).flatten
 
