77715b4f16d09940c7d043a98820908a7156cea3
Aravindh-Raju <aravindhraju2000@gmail.com>
2023-05-10 16:20:46 +0530

    add functional test

--- modules/api/src/test/functional/tests/batch/create_batch_change_test.py
+++ modules/api/src/test/functional/tests/batch/create_batch_change_test.py
@@ -4635,3 +4635,219 @@ def test_naptr_recordtype_update_delete_checks(shared_zone_test_context):
         ok_deletes = [rs for rs in to_delete if rs["zone"]["id"] != dummy_zone["id"]]
         clear_recordset_list(dummy_deletes, dummy_client)
         clear_recordset_list(ok_deletes, ok_client)
+
+
+def test_srv_recordtype_add_checks(shared_zone_test_context):
+    """
+    Test all add validations performed on SRV records submitted in batch changes
+    """
+    client = shared_zone_test_context.ok_vinyldns_client
+    ok_zone_name = shared_zone_test_context.ok_zone["name"]
+    dummy_zone_name = shared_zone_test_context.dummy_zone["name"]
+    dummy_group_name = shared_zone_test_context.dummy_group["name"]
+    ip4_zone_name = shared_zone_test_context.classless_base_zone["name"]
+
+    existing_srv_name = generate_record_name()
+    existing_srv_fqdn = f"{existing_srv_name}.{ok_zone_name}"
+    existing_srv = create_recordset(shared_zone_test_context.ok_zone, existing_srv_name, "SRV", [{"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}], 100)
+
+    existing_cname_name = generate_record_name()
+    existing_cname_fqdn = f"{existing_cname_name}.{ok_zone_name}"
+    existing_cname = create_recordset(shared_zone_test_context.ok_zone, existing_cname_name, "CNAME", [{"cname": "test."}], 100)
+
+    good_record_fqdn = generate_record_name(ok_zone_name)
+    batch_change_input = {
+        "changes": [
+            # valid change
+            get_change_SRV_json(good_record_fqdn, priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # input validation failures
+            get_change_SRV_json(f"bad-ttl-and-invalid-name$.{ok_zone_name}", ttl=29, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"srv.{ip4_zone_name}", priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # zone discovery failures
+            get_change_SRV_json(f"no.subzone.{ok_zone_name}", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json("no.zone.at.all.", priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # context validation failures
+            get_change_CNAME_json(f"cname-duplicate.{ok_zone_name}"),
+            get_change_SRV_json(f"cname-duplicate.{ok_zone_name}", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(existing_srv_fqdn, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(existing_cname_fqdn, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"user-add-unauthorized.{dummy_zone_name}", priority=1000, weight=5, port=20, target="bar.foo.")
+        ]
+    }
+
+    to_create = [existing_srv, existing_cname]
+    to_delete = []
+    try:
+        for create_json in to_create:
+            create_result = client.create_recordset(create_json, status=202)
+            to_delete.append(client.wait_until_recordset_change_status(create_result, "Complete"))
+
+        response = client.create_batch_change(batch_change_input, status=400)
+
+        # successful changes
+        assert_successful_change_in_error_response(response[0], input_name=good_record_fqdn, record_type="SRV", record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."})
+
+        # ttl, domain name, record data
+        assert_failed_change_in_error_response(response[1], input_name=f"bad-ttl-and-invalid-name$.{ok_zone_name}", ttl=29, record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=['Invalid TTL: "29", must be a number between 30 and 2147483647.',
+                                                               f'Invalid domain name: "bad-ttl-and-invalid-name$.{ok_zone_name}", '
+                                                               "valid domain names must be letters, numbers, underscores, and hyphens, joined by dots, and terminated with a dot."])
+        assert_failed_change_in_error_response(response[2], input_name=f"srv.{ip4_zone_name}", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=[f'Invalid Record Type In Reverse Zone: record with name "srv.{ip4_zone_name}" and type "SRV" is not allowed in a reverse zone.'])
+
+        # zone discovery failures
+        assert_failed_change_in_error_response(response[3], input_name=f"no.subzone.{ok_zone_name}", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=[f'Zone Discovery Failed: zone for "no.subzone.{ok_zone_name}" does not exist in VinylDNS. '
+                                                               f'If zone exists, then it must be connected to in VinylDNS.'])
+        assert_failed_change_in_error_response(response[4], input_name="no.zone.at.all.", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=['Zone Discovery Failed: zone for "no.zone.at.all." does not exist in VinylDNS. '
+                                                               'If zone exists, then it must be connected to in VinylDNS.'])
+
+        # context validations: cname duplicate
+        assert_failed_change_in_error_response(response[5], input_name=f"cname-duplicate.{ok_zone_name}", record_type="CNAME",
+                                               record_data="test.com.",
+                                               error_messages=[f"Record Name \"cname-duplicate.{ok_zone_name}\" Not Unique In Batch Change: "
+                                                               f"cannot have multiple \"CNAME\" records with the same name."])
+
+        # context validations: conflicting recordsets, unauthorized error
+        assert_successful_change_in_error_response(response[7], input_name=existing_srv_fqdn, record_type="SRV",
+                                                   record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."})
+        assert_failed_change_in_error_response(response[8], input_name=existing_cname_fqdn, record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=["CNAME Conflict: CNAME record names must be unique. "
+                                                               f"Existing record with name \"{existing_cname_fqdn}\" and type \"CNAME\" conflicts with this record."])
+        assert_failed_change_in_error_response(response[9], input_name=f"user-add-unauthorized.{dummy_zone_name}", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=[f"User \"ok\" is not authorized. Contact zone owner group: {dummy_group_name} at test@test.com to make DNS changes."])
+    finally:
+        clear_recordset_list(to_delete, client)
+
+
+def test_srv_recordtype_update_delete_checks(shared_zone_test_context):
+    """
+    Test all update and delete validations performed on SRV records submitted in batch changes
+    """
+    ok_client = shared_zone_test_context.ok_vinyldns_client
+    dummy_client = shared_zone_test_context.dummy_vinyldns_client
+    ok_zone = shared_zone_test_context.ok_zone
+    dummy_zone = shared_zone_test_context.dummy_zone
+    dummy_zone_name = shared_zone_test_context.dummy_zone["name"]
+
+    dummy_group_name = shared_zone_test_context.dummy_group["name"]
+    ok_zone_name = shared_zone_test_context.ok_zone["name"]
+    ip4_zone_name = shared_zone_test_context.classless_base_zone["name"]
+
+    rs_delete_name = generate_record_name()
+    rs_delete_fqdn = rs_delete_name + f".{ok_zone_name}"
+    rs_delete_ok = create_recordset(ok_zone, rs_delete_name, "SRV", [{"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}], 200)
+
+    rs_update_name = generate_record_name()
+    rs_update_fqdn = rs_update_name + f".{ok_zone_name}"
+    rs_update_ok = create_recordset(ok_zone, rs_update_name, "SRV", [{"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}], 200)
+
+    rs_delete_dummy_name = generate_record_name()
+    rs_delete_dummy_fqdn = rs_delete_dummy_name + f".{dummy_zone_name}"
+    rs_delete_dummy = create_recordset(dummy_zone, rs_delete_dummy_name, "SRV", [{"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}], 200)
+
+    rs_update_dummy_name = generate_record_name()
+    rs_update_dummy_fqdn = rs_update_dummy_name + f".{dummy_zone_name}"
+    rs_update_dummy = create_recordset(dummy_zone, rs_update_dummy_name, "SRV", [{"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}], 200)
+
+    batch_change_input = {
+        "comments": "this is optional",
+        "changes": [
+            # valid changes
+            get_change_SRV_json(rs_delete_fqdn, change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(rs_update_fqdn, change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(rs_update_fqdn, ttl=300, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"delete-nonexistent.{ok_zone_name}", change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"update-nonexistent.{ok_zone_name}", change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # input validations failures
+            get_change_SRV_json(f"invalid-name$.{ok_zone_name}", change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"delete.{ok_zone_name}", ttl=29, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(f"srv.{ip4_zone_name}", priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # zone discovery failures
+            get_change_SRV_json("no.zone.at.all.", change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+
+            # context validation failures
+            get_change_SRV_json(f"update-nonexistent.{ok_zone_name}", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(rs_delete_dummy_fqdn, change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(rs_update_dummy_fqdn, priority=1000, weight=5, port=20, target="bar.foo."),
+            get_change_SRV_json(rs_update_dummy_fqdn, change_type="DeleteRecordSet", priority=1000, weight=5, port=20, target="bar.foo.")
+        ]
+    }
+
+    to_create = [rs_delete_ok, rs_update_ok, rs_delete_dummy, rs_update_dummy]
+    to_delete = []
+
+    try:
+        for rs in to_create:
+            if rs["zoneId"] == dummy_zone["id"]:
+                create_client = dummy_client
+            else:
+                create_client = ok_client
+
+            create_rs = create_client.create_recordset(rs, status=202)
+            to_delete.append(create_client.wait_until_recordset_change_status(create_rs, "Complete"))
+
+        # Confirm that record set doesn't already exist
+        ok_client.get_recordset(ok_zone["id"], "delete-nonexistent", status=404)
+
+        response = ok_client.create_batch_change(batch_change_input, status=400)
+
+        # successful changes
+        assert_successful_change_in_error_response(response[0], input_name=rs_delete_fqdn, record_type="SRV", record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}, change_type="DeleteRecordSet")
+        assert_successful_change_in_error_response(response[1], input_name=rs_update_fqdn, record_type="SRV", record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}, change_type="DeleteRecordSet")
+        assert_successful_change_in_error_response(response[2], ttl=300, input_name=rs_update_fqdn, record_type="SRV", record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."})
+        assert_successful_change_in_error_response(response[3], input_name=f"delete-nonexistent.{ok_zone_name}", record_type="SRV",
+                                                   record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}, change_type="DeleteRecordSet")
+        assert_successful_change_in_error_response(response[4], input_name=f"update-nonexistent.{ok_zone_name}", record_type="SRV",
+                                                   record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}, change_type="DeleteRecordSet")
+
+        # input validations failures: invalid input name, reverse zone error, invalid ttl
+        assert_failed_change_in_error_response(response[5], input_name=f"invalid-name$.{ok_zone_name}", record_type="SRV", record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               change_type="DeleteRecordSet",
+                                               error_messages=[f'Invalid domain name: "invalid-name$.{ok_zone_name}", valid domain names must be letters, '
+                                                               f'numbers, underscores, and hyphens, joined by dots, and terminated with a dot.'])
+        assert_failed_change_in_error_response(response[6], input_name=f"delete.{ok_zone_name}", ttl=29, record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=['Invalid TTL: "29", must be a number between 30 and 2147483647.'])
+        assert_failed_change_in_error_response(response[7], input_name=f"srv.{ip4_zone_name}", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=[f'Invalid Record Type In Reverse Zone: record with name "srv.{ip4_zone_name}" '
+                                                               f'and type "SRV" is not allowed in a reverse zone.'])
+
+        # zone discovery failure
+        assert_failed_change_in_error_response(response[8], input_name="no.zone.at.all.", record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."}, change_type="DeleteRecordSet",
+                                               error_messages=["Zone Discovery Failed: zone for \"no.zone.at.all.\" does not exist in VinylDNS. "
+                                                               "If zone exists, then it must be connected to in VinylDNS."])
+
+        # context validation failures: record does not exist, not authorized
+        assert_successful_change_in_error_response(response[9], input_name=f"update-nonexistent.{ok_zone_name}", record_type="SRV",
+                                                   record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."})
+        assert_failed_change_in_error_response(response[10], input_name=rs_delete_dummy_fqdn, record_type="SRV",
+                                               record_data=None, change_type="DeleteRecordSet",
+                                               error_messages=[f"User \"ok\" is not authorized. Contact zone owner group: {dummy_group_name} at test@test.com to make DNS changes."])
+        assert_failed_change_in_error_response(response[11], input_name=rs_update_dummy_fqdn, record_type="SRV",
+                                               record_data={"priority": 1000, "weight": 5, "port": 20, "target": "bar.foo."},
+                                               error_messages=[f"User \"ok\" is not authorized. Contact zone owner group: {dummy_group_name} at test@test.com to make DNS changes."])
+        assert_failed_change_in_error_response(response[12], input_name=rs_update_dummy_fqdn, record_type="SRV",
+                                               record_data=None, change_type="DeleteRecordSet",
+                                               error_messages=[f"User \"ok\" is not authorized. Contact zone owner group: {dummy_group_name} at test@test.com to make DNS changes."])
+    finally:
+        # Clean up updates
+        dummy_deletes = [rs for rs in to_delete if rs["zone"]["id"] == dummy_zone["id"]]
+        ok_deletes = [rs for rs in to_delete if rs["zone"]["id"] != dummy_zone["id"]]
+        clear_recordset_list(dummy_deletes, dummy_client)
+        clear_recordset_list(ok_deletes, ok_client)
+
