1c51150b560918aabf4cd30ad0cb3141ba9e2d84
nspadaccino <nicholas_spadaccino@comcast.com>
2023-10-03 17:04:03 -0400

    Create procedure to add columns only if they don't exist already

--- modules/mysql/src/main/resources/db/migration/V3.29__AddRecordChangeFields.sql
+++ modules/mysql/src/main/resources/db/migration/V3.29__AddRecordChangeFields.sql
@@ -2,7 +2,27 @@ CREATE SCHEMA IF NOT EXISTS ${dbName};
 
 USE ${dbName};
 
-ALTER TABLE record_change ADD COLUMN IF NOT EXISTS fqdn VARCHAR(255) NOT NULL,
-ALTER TABLE record_change ADD COLUMN IF NOT EXISTS record_type VARCHAR(255) NOT NULL;
+DELIMITER $$
+
+CREATE PROCEDURE AddColumnIfNotExists(IN columnName VARCHAR(255))
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'record_change'
+        AND column_name = columnName
+    ) THEN
+        SET @sql = CONCAT('ALTER TABLE record_change ADD COLUMN ', columnName, ' VARCHAR(255) NOT NULL');
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END $$
+
+DELIMITER ;
+
+CALL AddColumnIfNotExists('record_type');
+CALL AddColumnIfNotExists('fqdn');
+
 CREATE INDEX fqdn_index ON record_change (fqdn);
 CREATE INDEX record_type_index ON record_change (record_type);
