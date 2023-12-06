ebf1dbb2aefc4882f7cfc626cc76cdbcdefac6c9
Jay07GIT <jeyraj931@gmail.com>
2023-12-06 19:07:10 +0530

    added index for zone change columns

--- modules/mysql/src/main/resources/db/migration/V3.31__AddZoneChangeFields.sql
+++ modules/mysql/src/main/resources/db/migration/V3.31__AddZoneChangeFields.sql
@@ -4,6 +4,9 @@ USE ${dbName};
 
 ALTER TABLE zone_change
 ADD COLUMN zone_name VARCHAR(256) NOT NULL;
+CREATE INDEX zone_name_index ON zone_change(zone_name);
+
 
 ALTER TABLE zone_change
 ADD COLUMN zone_status CHAR(36) NOT NULL;
+CREATE INDEX zone_status_index ON zone_change(zone_status);
