dbe9fcacda850cb77828464251cdaf69f23b4570
Jay07GIT <jeyraj931@gmail.com>
2025-09-12 15:31:01 +0530

    update
    Signed-off-by: Jay07GIT <jeyraj931@gmail.com>

--- modules/api/src/test/functional/tests/shared_zone_test_context.py
+++ modules/api/src/test/functional/tests/shared_zone_test_context.py
@@ -82,7 +82,12 @@ class SharedZoneTestContext(object):
                 "description": "this is a description",
                 "members": [{"id": "ok"}, {"id": "support-user-id"}],
                 "admins": [{"id": "ok"}],
-                "membershipAccessStatus": ""
+                "membershipAccessStatus": {
+                    "pendingReviewMember": [],
+                    "rejectedMember": [],
+                    "approvedMember": []
+                }
+
             }
 
             self.ok_group = self.ok_vinyldns_client.create_group(ok_group, status=200)
@@ -95,7 +100,12 @@ class SharedZoneTestContext(object):
                 "description": "this is a description",
                 "members": [{"id": "dummy"}],
                 "admins": [{"id": "dummy"}],
-                "membershipAccessStatus": ""
+                "membershipAccessStatus": {
+                    "pendingReviewMember": [],
+                    "rejectedMember": [],
+                    "approvedMember": []
+                }
+
             }
             self.dummy_group = self.dummy_vinyldns_client.create_group(dummy_group, status=200)
             # in theory this shouldn"t be needed, but getting "user is not in group' errors on zone creation
@@ -107,7 +117,12 @@ class SharedZoneTestContext(object):
                 "description": "this is a description",
                 "members": [{"id": "sharedZoneUser"}, {"id": "ok"}, {"id": "support-user-id"}],
                 "admins": [{"id": "sharedZoneUser"}, {"id": "ok"}],
-                "membershipAccessStatus": ""
+                "membershipAccessStatus": {
+                    "pendingReviewMember": [],
+                    "rejectedMember": [],
+                    "approvedMember": []
+                }
+
             }
             self.shared_record_group = self.ok_vinyldns_client.create_group(shared_record_group, status=200)
 
@@ -117,7 +132,12 @@ class SharedZoneTestContext(object):
                 "description": "this is a description",
                 "members": [{"id": "history-id"}],
                 "admins": [{"id": "history-id"}],
-                "membershipAccessStatus": ""
+                "membershipAccessStatus": {
+                    "pendingReviewMember": [],
+                    "rejectedMember": [],
+                    "approvedMember": []
+                }
+
             }
             self.history_group = self.history_client.create_group(history_group, status=200)
             self.confirm_member_in_group(self.history_client, self.history_group)
