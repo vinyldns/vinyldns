bce72df67c64d0f8ee69b99812affbf6ae4c5b81
Sokitha-21 <krishnansokitha@gmail.com>
2026-03-06 23:50:35 +0530

    validate zoneid in create recordset api

--- modules/api/src/main/scala/vinyldns/api/route/RecordSetRouting.scala
+++ modules/api/src/main/scala/vinyldns/api/route/RecordSetRouting.scala
@@ -32,6 +32,7 @@ import vinyldns.core.domain.zone.ZoneCommandResult
 import akka.http.scaladsl.model.HttpEntity
 import spray.json._
 
+
 import scala.concurrent.duration._
 
 case class GetRecordSetResponse(recordSet: RecordSetInfo)
@@ -92,7 +93,14 @@ class RecordSetRoute(
   val recordSetRoute: Route = path("zones" / Segment / "recordsets") { zoneId =>
     (post & monitor("Endpoint.addRecordSet")) {
       authenticateAndExecuteWithEntity[ZoneCommandResult, RecordSet](
-        (authPrincipal, recordSet) => recordSetService.addRecordSet(recordSet, authPrincipal)
+        (authPrincipal, recordSet) => {
+          if (recordSet.zoneId.nonEmpty && recordSet.zoneId != zoneId) {
+            Left(RecordSetValidation("zoneId in URI and body must match")).toResult
+          } else {
+            val updatedRecordSet = recordSet.copy(zoneId = zoneId)
+            recordSetService.addRecordSet(updatedRecordSet, authPrincipal)
+          }
+        }
       ) { rc =>
         complete(StatusCodes.Accepted, rc)
       }
