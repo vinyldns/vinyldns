c9a55a1d1e765e23020725917974f4a9b1f22f6e
Sokitha-21 <krishnansokitha@gmail.com>
2026-03-09 15:01:00 +0530

    added tests
    added tests
    
    added tests
    
    added tests
    
    added tests
    
    added tests

--- modules/api/src/main/scala/vinyldns/api/route/RecordSetRouting.scala
+++ modules/api/src/main/scala/vinyldns/api/route/RecordSetRouting.scala
@@ -93,14 +93,14 @@ class RecordSetRoute(
   val recordSetRoute: Route = path("zones" / Segment / "recordsets") { zoneId =>
     (post & monitor("Endpoint.addRecordSet")) {
       authenticateAndExecuteWithEntity[ZoneCommandResult, RecordSet](
-        (authPrincipal, recordSet) => {
-          if (recordSet.zoneId.nonEmpty && recordSet.zoneId != zoneId) {
-            Left(RecordSetValidation("zoneId in URI and body must match")).toResult
-          } else {
-            val updatedRecordSet = recordSet.copy(zoneId = zoneId)
-            recordSetService.addRecordSet(updatedRecordSet, authPrincipal)
+        (authPrincipal, recordSet) =>   
+          recordSet match {
+            case badRs if badRs.zoneId.nonEmpty && badRs.zoneId != zoneId =>
+              Left(InvalidRequest("zoneId in URI and body must match")).toResult
+            case goodRs =>
+              val updatedRecordSet = goodRs.copy(zoneId = zoneId)
+              recordSetService.addRecordSet(updatedRecordSet, authPrincipal)
           }
-        }
       ) { rc =>
         complete(StatusCodes.Accepted, rc)
       }
