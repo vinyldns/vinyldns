12a226e94b5166fd983600214af88f7f6c458720
Jay07GIT <jeyraj931@gmail.com>
2026-05-26 18:56:28 +0530

    trigger tests
    Signed-off-by: Jay07GIT <jeyraj931@gmail.com>

--- modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlBatchChangeRepository.scala
+++ modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlBatchChangeRepository.scala
@@ -289,7 +289,6 @@ class MySqlBatchChangeRepository
 
           val opts = Seq(groupFilter, uid, as, bs, uname, dtRange).flatten
           if (opts.nonEmpty) sb.append("WHERE ").append(opts.mkString(" AND "))
-
           sb.append(GET_BATCH_CHANGE_SUMMARY_END)
           val query = sb.toString()
 
