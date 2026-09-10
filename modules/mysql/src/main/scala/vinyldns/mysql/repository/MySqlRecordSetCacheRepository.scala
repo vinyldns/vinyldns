c5a706b14f9e1ca1fe407560198279c2a28291e5
Sokitha-21 <krishnansokitha@gmail.com>
2026-09-10 15:17:40 +0530

    resolved comments

--- modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlRecordSetCacheRepository.scala
+++ modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlRecordSetCacheRepository.scala
@@ -403,6 +403,7 @@ class MySqlRecordSetCacheRepository
                   ON recordset.id = recordset_data.recordset_id
               """
           val countOpts = (zoneAndNameFilters ++ typeFilter ++ ownerGroupFilter ++ authFilter).toList
+          
           val countWhere =
             if (countOpts.nonEmpty) {
               val setDelimiter = SQLSyntax.join(countOpts, sqls"AND")
